Fixes: ether orbitals, rotation error in embedding

main.py
@@ -8,7 +8,6 @@ from hypermolecule_class import Hypermolecule, pt
 import numpy as np
 from copy import deepcopy
 from parameters import *
-from scipy.spatial.transform import Rotation as R
 import os
 import time
 from cclib.io import ccread
@@ -19,9 +18,8 @@ from ase.optimize import BFGS
 from ase.constraints import FixAtoms
 from ase.calculators.mopac import MOPAC
 from optimization_methods import *
-
-
 from subprocess import DEVNULL, STDOUT, check_call
+from linalg_tools import *
 
 class ZeroCandidatesError(Exception):
     pass
@@ -31,6 +29,7 @@ class InputError(Exception):
 
 stamp = time.ctime().replace(' ','_').replace(':','-')
 log = open(f'TSCoDe_log_{stamp}.txt', 'a', buffering=1)
+
 def log_print(string=''):
     print(string)
     string += '\n'
@@ -43,12 +42,6 @@ def loadbar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill=
 	print(f'\r{prefix} |{bar}| {percent}% {suffix}', end='\r')
 	if iteration == total:
 		print()
-
-def norm(vec):
-    return vec / np.linalg.norm(vec)
-
-def cartesian_product(*arrays):
-    return np.stack(np.meshgrid(*arrays), -1).reshape(-1, len(arrays))
 
 def calc_positioned_conformers(self):
     self.positioned_conformers = np.array([[self.rotation @ v + self.position for v in conformer] for conformer in self.atomcoords])
@@ -66,89 +59,6 @@ def write_xyz(coords:np.array, atomnos:np.array, output, title='TEST'):
     for i, atom in enumerate(coords):
         string += '%s     % .6f % .6f % .6f\n' % (pt[atomnos[i]].symbol, atom[0], atom[1], atom[2])
     output.write(string)
-
-def rotation_matrix_from_vectors(vec1, vec2):
-    """
-    Find the rotation matrix that aligns vec1 to vec2
-    :param vec1: A 3d "source" vector
-    :param vec2: A 3d "destination" vector
-    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
-
-    """
-    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
-    v = np.cross(a, b)
-    c = np.dot(a, b)
-    s = np.linalg.norm(v)
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
-    return rotation_matrix
-
-def rot_mat_from_pointer(pointer, angle):
-    '''
-    Returns the rotation matrix that rotates a system around the given pointer
-    of angle degrees. The algorithm is based on scipy quaternions.
-    :params pointer: a 3D vector
-    :params angle: a int/float, in degrees
-    :return rotation_matrix: matrix that applied to a point, rotates it along the pointer
-    '''
-    assert pointer.shape[0] == 3
-
-    pointer = norm(pointer)
-    angle *= np.pi/180
-
-    quat = np.array([np.sin(angle/2)*pointer[0],
-                    np.sin(angle/2)*pointer[1],
-                    np.sin(angle/2)*pointer[2],
-                    np.cos(angle/2)])            # normalized quaternion, scalar last (i j k w)
-
-    return R.from_quat(quat).as_matrix()
-
-def polygonize(lengths):
-    '''
-    Returns coordinates for the polygon vertexes used in cyclical TS construction,
-    as a list of vector couples specifying starting and ending point of each pivot 
-    vector. For bimolecular TSs, returns vertexes for the centered superposition of
-    two segments. For trimolecular TSs, returns triangle vertexes.
-
-    :params vertexes: list of floats, used as polygon side lenghts.
-    :return vertexes_out: list of vectors couples (start, end)
-    '''
-    assert len(lengths) in (2,3)
-    lengths = sorted(lengths)
-    arr = np.zeros((len(lengths),2,3))
-
-    if len(lengths) == 2:
-        arr[0,0] = np.array([-lengths[0]/2,0,0])
-        arr[0,1] = np.array([+lengths[0]/2,0,0])
-        arr[1,0] = np.array([-lengths[1]/2,0,0])
-        arr[1,1] = np.array([+lengths[1]/2,0,0])
-
-        vertexes_out = np.vstack(([arr],[arr]))
-        vertexes_out[1,1] *= -1
-
-    else:
-        arr[0,1] = np.array([lengths[0],0,0])
-        arr[1,0] = np.array([lengths[0],0,0])
-
-        a = np.power(lengths[0], 2)
-        b = np.power(lengths[1], 2)
-        c = np.power(lengths[2], 2)
-        x = (a-b+c)/(2*a**0.5)
-        y = (c-x**2)**0.5
-
-        arr[1,1] = np.array([x,y,0])
-        arr[2,0] = np.array([x,y,0])
-
-        vertexes_out = np.vstack(([arr],[arr],[arr],[arr],
-                                  [arr],[arr],[arr],[arr]))
-
-        swaps = [(1,2),(2,1),(3,1),(3,2),(4,0),(5,0),(5,1),(6,0),(6,2),(7,0),(7,1),(7,2)]
-
-        for t,v in swaps:
-            # triangle, vector couples to be swapped
-            vertexes_out[t,v][[0,1]] = vertexes_out[t,v][[1,0]]
-
-    return vertexes_out
 
 class Docker:
     def __init__(self, *objects):
@@ -193,14 +103,14 @@ class Docker:
                     set_pivots(molecule)
 
                 self.embed = 'cyclical'
-                self.candidates = self.rotation_steps**len(self.objects)*np.prod([len(mol.coords) for mol in self.object])
-                self.candidates *= np.prod([len(mol.pivots) for mol in self.object])*8
+                self.candidates = self.rotation_steps**len(self.objects)*np.prod([len(mol.atomcoords) for mol in self.objects])
+                self.candidates *= np.prod([len(mol.pivots) for mol in self.objects])*8
                 # The number 8 is the number of different triangles originated from three oriented vectors
 
 
             elif all([len(molecule.reactive_atoms_classes) == 1 for molecule in self.objects]) and len(self.objects) == 2:
                 self.embed = 'string'
-                self.candidates = self.rotation_steps**len(self.objects)*np.prod([len(mol.coords) for mol in self.object])
+                self.candidates = self.rotation_steps**len(self.objects)*np.prod([len(mol.atomcoords) for mol in self.objects])
                 self.candidates *= 2
                 # The number 2 is the number of different arrangementsof two oriented vectors (parallel, antiparallel)
 
@@ -381,16 +291,17 @@ class Docker:
                         # for the pivot vector
 
                             start, end = vec_pair
-
                             angle = angles[i]
+
                             alignment_rotation = rotation_matrix_from_vectors(pivots[i], end-start)
+
                             step_rotation = rot_mat_from_pointer(alignment_rotation @ pivots[i], angle)
                             center_of_rotation = np.mean(vec_pair, axis=0)
 
                             thread[i].rotation = step_rotation @ alignment_rotation
 
-                            pos = center_of_rotation - alignment_rotation @ pivot_means[i]
-                            thread[i].position = step_rotation @ (pos + center_of_rotation) - center_of_rotation                
+                            pos = center_of_rotation - thread[i].rotation @ pivot_means[i]
+                            thread[i].position = step_rotation @ (pos - center_of_rotation) + center_of_rotation
 
                 else:
                     log_print('# TO DO: Rejected embed: not matching imposed criterion')
@@ -467,7 +378,8 @@ class Docker:
             mask = np.zeros(len(self.structures), dtype=bool)
             num = len(self.structures)
             for s, structure in enumerate(self.structures):
-                if s % (num // 100) == 0:
+                p = True if num > 100 and s % (num // 100) == 0 else False
+                if p:
                     loadbar(s, num, prefix=f'Checking structure {s+1}/{num} ')
                 mask[s] = sanity_check(structure, atomnos, self.constrained_indexes[s], graphs, max_new_bonds=3)
             loadbar(1, 1, prefix=f'Checking structure {len(self.structures)}/{len(self.structures)} ')
@@ -601,17 +513,18 @@ if __name__ == '__main__':
         pass
 
     # a = ['Resources/SN2/MeOH_ensemble.xyz', 1]
-    a = ['Resources/dienamine/dienamine_ensemble.xyz', 6]
+    # a = ['Resources/dienamine/dienamine_ensemble.xyz', 6]
     # a = ['Resources/SN2/amine_ensemble.xyz', 22]
     # a = ['Resources/indole/indole_ensemble.xyz', 6]
     ######################################################
-    # b = ['Resources/SN2/CH3Br_ensemble.xyz', 0]
-    b = ['Resources/SN2/ketone_ensemble.xyz', 2]
+    b = ['Resources/SN2/CH3Br_ensemble.xyz', 0]
+    # b = ['Resources/SN2/ketone_ensemble.xyz', 2]
     # inp = [a,b]
 
-    c = ['Resources/SN2/MeOH_ensemble.xyz', (1,5)]
+    # c = ['Resources/SN2/MeOH_ensemble.xyz', (1,5)]
     # c = ['Resources/DA/CHCH3.xyz', [0,6]]
-    inp = [c,c,c]
+    # c = ['Resources/SN2/BrCl.xyz', [0,1]]
+    # inp = [c,c,c]
 
     # d = ['Resources/DA/diene.xyz', (2,7)]
     # e = ['Resources/DA/dienophile.xyz', (3,5)]
@@ -622,12 +535,16 @@ if __name__ == '__main__':
     # e = ['Resources/DA/dienophile2.xyz', (3,5)]
     # inp = [d,e]
 
+    a = ['Resources/dienamine/dienamine_ensemble.xyz', (6,23)]
+    b = ['Resources/acid_ensemble.xyz', (3,25)]
+    c = ['Resources/maleimide.xyz', (0,5)]
+    inp = (a,b,c)
 
 
     objects = [Hypermolecule(m[0], m[1]) for m in inp]
 
     docker = Docker(objects) # initialize docker with molecule density objects
-    docker.setup(steps=2, optimize=False) # set variables
+    docker.setup(steps=3, optimize=False) # set variables
 
     os.chdir('Resources/SN2')
 

reactive_atoms_classes.py
@@ -2,9 +2,8 @@ import numpy as np
 from parameters import *
 from scipy import ndimage
 from scipy.spatial.transform import Rotation as R
-
-def norm(vec):
-    return vec / np.linalg.norm(vec)
+# TO DO: change with alignment function from linalg_tools
+from linalg_tools import *
 
 class Single:
     '''
@@ -42,7 +41,7 @@ class Single:
             print(f'ATTENTION: COULD NOT SETUP REACTIVE ATOM ORBITAL FROM PARAMETERS. We have no parameters for {key}. Using half the bonding distance.')
 
         self.center = np.array([orb_dim * norm(self.coord - self.others) + self.coord])
-        return None
+
 
 class Sp2:
     '''
@@ -83,8 +82,6 @@ class Sp2:
 
         self.center += self.coord
 
-        return None
-
 
 class Sp: # BROKEN for sure, needs to fixed, eventually
     '''
@@ -108,8 +105,6 @@ class Sp: # BROKEN for sure, needs to fixed, eventually
         self.others = bonded_atom_coords
         self.vectors = self.others - self.coord # vectors connecting reactive atom with neighbors
        
-        return None
-
 
 class Sp3:
     '''
@@ -155,8 +150,6 @@ class Sp3:
         self.center = np.array([orb_dim * norm(self.orb_vec) + self.coord])
 
         self.alignment_matrix = R.align_vectors(np.array([[1,0,0]]), np.array([self.center[0]]))[0].as_matrix()
-
-        return None
 
     def _set_leaving_group(self, filename, neighbors_indexes):
         '''
@@ -204,6 +197,7 @@ class Sp3:
 
         return leaving_group_coords
 
+
 class Ether:
     '''
     '''
@@ -239,25 +233,25 @@ class Ether:
 
         self.vectors = orb_dim * np.array([norm(v) for v in self.vectors]) # making both vectors a fixed, defined length
         
-        self.alignment_matrix = R.align_vectors(np.array([[1,0,0]]), np.array([-np.mean(self.vectors, axis=0)]))[0].as_matrix()
+        self.alignment_matrix = rotation_matrix_from_vectors(np.array([1,0,0]), -np.mean(self.vectors, axis=0))
 
-        alignment_on_z = R.align_vectors(np.array([[0,0,1]]), np.array([self.vectors[0]]))[0].as_matrix()
 
-        rotoreflexion_matrix = np.array([[ np.cos(2*np.pi/4), np.sin(2*np.pi/4),  0],
-                                         [-np.sin(2*np.pi/4), np.cos(2*np.pi/4),  0],
-                                         [                 0,                 0, -1]])
+        orb_mat = rot_mat_from_pointer(np.mean(self.vectors, axis=0), 90) @ rot_mat_from_pointer(norm(np.cross(self.vectors[0], self.vectors[1])), 180)
 
-        self.center = np.array([v @ alignment_on_z @ rotoreflexion_matrix @ np.linalg.inv(alignment_on_z) for v in self.vectors])
+        self.center = np.array([orb_mat @ v for v in self.vectors])
+        
         self.center += self.coord
         # two vectors defining the position of the two orbital lobes centers
 
 
-        return None
+class Ketone:
+    '''
+    '''
 
 atom_type_dict = {
              'H1' : Single(),
-             'C1' : Single(),
-             'C2' : Sp(), # toroidal geometry
+             'C1' : Single(), # deprotonated terminal alkyne?
+            #  'C2' : Sp(), # toroidal geometry (or carbene, or vinyl anion - #panik)
              'C3' : Sp2(), # double ball
              'C4' : Sp3(), # one ball: on the back of weakest bond. If can't tell which is which, one big ball
              'N1' : Single(),