3fba483777715107a2d9a78d7305864626ec6103
andre-cloud <55535285+andre-cloud@users.noreply.github.com>
2021-04-15 17:04:12 +0200

    Add darwin.so

--- cython/compenetration/setup.py
+++ cython/compenetration/setup.py
@@ -1,7 +1,9 @@
-import setuptools
+import setuptools, os
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy as np
 
+path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(path)
 setup(ext_modules = cythonize('compenetration.pyx'),
       include_dirs=[np.get_include()])
